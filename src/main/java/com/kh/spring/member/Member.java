8fb11c13d4a0932d4a30d0f9c531c8d58eb7f3e7
sunaaaaaaaa <tjsdk7075@naver.com>
2021-12-18 16:13:07 +0900

    [최선아]산타 프로젝트 스프링부트

---
+++ src/main/java/com/kh/spring/member/Member.java
@@ -0,0 +1,34 @@
+package com.kh.spring.member;
+
+import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import lombok.Data;
+
+@Data
+@Entity
+@DynamicInsert
+@DynamicUpdate
+public class Member {
+	
+   @Id
+   private String userId;
+   private String password;
+   private String email;
+   private String grade;
+   private String tell;
+   
+   @Column(columnDefinition = "date default sysdate")
+   private LocalDate rentableDate;
+   
+   @Column(columnDefinition = "date default sysdate")
+   private LocalDate regDate;
+   
+   @Column(columnDefinition = "number default 0")
+   private Boolean isLeave;
+
+}
---
+++ src/main/java/com/kh/spring/member/MemberRepository.java
@@ -0,0 +1,7 @@
+package com.kh.spring.member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, String>{
+	
+}
